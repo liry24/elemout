553cd37dd7cf5b40b628ef66326ac17ce2d6278f
Liry24 <liry241999@gmail.com>
2026-05-15 16:29:49 +0900

    chore: release v0.1.0

--- app.config.ts
+++ app.config.ts
@@ -1,5 +1,5 @@
 export default {
-    version: '0.0.0',
+    version: '0.1.0',
     name: 'Elemout',
     repo: 'https://github.com/liry24/elemout',
     donate: 'https://github.com/sponsors/liry24',
